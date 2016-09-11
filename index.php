178dac8aafc2bc94ba16c35cc9365ba5179d6c22
Danilo Mendonca <favoratti@gmail.com>
2016-09-11 13:39:31 -0300

    Alterando ID da TAG de exemplo

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   			<p><input name="login" type="text" placeholder="Digite seu usuário do KlickMail"></p>
   			<p><input name="password" type="password" placeholder="Senha"></p>
   			<p><input name="poId" type="text" placeholder="Digite o ID do seu processo de dupla inscrição">&nbsp;Ex.: https://www.klickmail.com.br/lists/21358/<b>48970</b>/edit</p>
-  			<p><input name="tagId" type="text" placeholder="Digite o ID da TAG">&nbsp;Ex.: https://www.klickmail.com.br/tags/21358/edit/<b>784142</b></p>
+  			<p><input name="tagId" type="text" placeholder="Digite o ID da TAG">&nbsp;Ex.: https://www.klickmail.com.br/tags/21358/edit/<b>810798</b></p>
   			<p><textarea name="emails" rows="15" cols="100"></textarea></p>
   			<p><input type="submit" value="Cadastrar e-mails"></p>
   		</form>
